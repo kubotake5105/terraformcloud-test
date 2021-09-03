d39f509b329c61246a2dcf56908aff0f8a8eaf1a
Your Name <you@example.com>
2021-09-03 17:35:05 +0900

    ファイル調整2

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
-provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region = "ap-northeast-1"
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "aws_region" {
+default = "ap-northeast-1"
 }
